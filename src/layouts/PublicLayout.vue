f07b752a1eb3a96c7179fc0077efc165787b7887
Abulkosim <abulkasimkattaev@gmail.com>
2025-07-20 15:32:49 +0500

    feat: public notes layout

--- src/layouts/PublicLayout.vue
+++ src/layouts/PublicLayout.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppHeader from '../components/AppHeader.vue'
 </script>
 
 <template>
   <div class="public-layout">
+    <AppHeader />
+
     <RouterView />
   </div>
 </template>
